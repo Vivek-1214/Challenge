0dbbb59dc3fcfe211ff60a5a7b80dce4b4f2d181
deepak shinde <deepak shinde@DELL>
2023-03-18 09:01:03 +0530

    Add project files.

---
+++ Challenge/Program.cs
@@ -0,0 +1,24 @@
+﻿//Question
+//Accept any number from user and return the factors of that number
+//If user enters number 10 then factors will be 
+
+using System;
+namespace Challenges { 
+    class Demo {
+
+        static void Main()
+        {
+            Console.WriteLine("Enter any number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("factors of a " + num);
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+}
+}
