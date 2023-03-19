8b060ec94bcf10f0b45e7a6341ab027b64d0fe01
deepak shinde <deepak shinde@DELL>
2023-03-19 14:43:28 +0530

    new challenges

--- Challenge/Program.cs
+++ Challenge/Program.cs
@@ -8,17 +8,127 @@ namespace Challenges {
 
         static void Main()
         {
-            Console.WriteLine("Enter any number");
-            int num = Convert.ToInt32(Console.ReadLine());
+            // Factorials();
+            //odd();
+            //prime();
+            //Table();
+            DecimalToBinary();
+            }
+        static void Factorials()
+        {
+            Console.WriteLine("Please Enter start of limit");
+            int startOfLimit = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("factors of a " + num);
-            for (int i = 1; i <= num; i++)
+            Console.WriteLine("Please Enter end limit");
+            int endOfLimit = Convert.ToInt32(Console.ReadLine());
+
+          
+           // Console.WriteLine($"factors of a {num} =");
+            for (int j = startOfLimit; j <= endOfLimit; j++)
             {
-                if (num % i == 0)
+                int fact = 1, i = 1;
+
+                for (i = 1; i <= j; i++)
                 {
-                    Console.WriteLine(i);
+                    fact = fact * i;
                 }
+                Console.WriteLine(fact);
             }
         }
-}
+            static void odd()
+            {
+                Console.WriteLine("Enter any number :");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("odd numbers are : ");
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i+" ");
+                }
+            }
+            }
+        static void prime()
+        {
+            Console.WriteLine("Enter start of limit :");
+            int start = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the limit :");
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("prime numbers are :");
+           
+            for (int j = start; j <= limit; j++)
+            {
+                int count = 0;
+                for (int i = 1; i <= j; i++)
+                {
+                    if (j % i == 0)
+                    {
+                        count++;
+                    }
+                }
+
+                if (count <= 2)
+                {
+                    Console.Write(j+" ");
+                }
+            }
+          
+           
+
+        }
+
+        static void Table()
+        {
+            Console.WriteLine("Please Enter start of limit");
+            int startOfLimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter endOfLimit");
+            int endOfLimit = Convert.ToInt32(Console.ReadLine());
+
+            for (int j = startOfLimit; j <= endOfLimit; j++)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    int tab = j * i;
+                    Console.Write(tab+" ");
+                }
+                Console.WriteLine();
+            }
+           
+        }
+
+        static void DecimalToBinary()
+        {
+            Console.WriteLine("Please Enter Decimal number");
+            int num1 = Convert.ToInt32(Console.ReadLine());  //13
+
+            int rem = 0; 
+           
+            int[] arr = new int[10];
+            int i = 0;
+            while ( num1 != 0)
+            {
+                rem = num1 % 2;
+                num1  =num1 / 2;
+
+               arr[i++]= rem;
+               
+            }
+             Array.Reverse(arr);
+            try
+            {
+                for (int j = 0; j <= arr.Length; j++)
+                {
+                    Console.Write(arr[j] + " ");
+                }
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+
+            }
+        }
+     }
 }
